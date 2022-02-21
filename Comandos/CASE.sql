921375b46643fb5cfa59df3cc2498aabbd2e4446
YagoAmaral <100030153+YagoAmaral@users.noreply.github.com>
2022-02-21 19:53:12 -0300

    Update CASE.sql

--- Comandos/CASE.sql
+++ Comandos/CASE.sql
@@ -1,26 +1,31 @@
 SELECT * FROM tabela_de_clientes;
-SELECT estado, limite_de_credito from tabela_de_clientes;
-SELECT estado, sum(limite_de_credito) as Limite_total from tabela_de_clientes group by estado;
+
+SELECT estado, limite_de_credito FROM tabela_de_clientes;
+
+SELECT estado, sum(limite_de_credito) AS Limite_total FROM tabela_de_clientes GROUP BY estado;
+
 SELECT EMBALAGEM, MAX(PRECO_DE_LISTA) AS MAIOR_PRECO FROM tabela_de_produtos GROUP BY EMBALAGEM;
+
 SELECT EMBALAGEM, count(*) AS TOTAL FROM tabela_de_produtos GROUP BY EMBALAGEM;
+
 SELECT BAIRRO, SUM(LIMITE_DE_CREDITO) AS LIMITE FROM TABELA_DE_CLIENTES 
-WHERE CIDADE = 'RIO DE JANEIRO' GROUP BY BAIRRO order by LIMITE;
+WHERE CIDADE = 'RIO DE JANEIRO' GROUP BY BAIRRO ORDER BY LIMITE;
 SELECT ESTADO, SUM(LIMITE_DE_CREDITO) AS SOMA FROM tabela_de_clientes 
 GROUP BY ESTADO HAVING SUM(LIMITE_DE_CREDITO) > 900000;
 
-select nome_do_produto, preco_de_lista,
-case 
-when preco_de_lista >= 12 then 'Produto Caro'
-when preco_de_lista >= 7 and preco_de_lista < 12 then 'Em Conta'
-Else 'Produto Barato' end as Status_Preco from tabela_de_produtos;
+SELECT nome_do_produto, preco_de_lista,
+CASE 
+WHEN preco_de_lista >= 12 THEN 'Produto Caro'
+WHEN preco_de_lista >= 7 AND preco_de_lista < 12 THEN 'Em Conta'
+ELSE 'Produto Barato' END AS Status_Preco FROM tabela_de_produtos;
 
-select EMBALAGEM,
-case 
-when preco_de_lista >= 12 then 'Produto Caro'
-when preco_de_lista >= 7 and preco_de_lista < 12 then 'Em Conta'
-Else 'Produto Barato' end as Status_Preco, AVG(PRECO_DE_LISTA) AS PRECO_MEDIO
-from tabela_de_produtos GROUP BY EMBALAGEM,
-case 
-when preco_de_lista >= 12 then 'Produto Caro'
-when preco_de_lista >= 7 and preco_de_lista < 12 then 'Em Conta'
-Else 'Produto Barato' end order by embalagem;
+SELECT EMBALAGEM,
+CASE 
+WHEN preco_de_lista >= 12 THEN 'Produto Caro'
+WHEN preco_de_lista >= 7 AND preco_de_lista < 12 THEN 'Em Conta'
+ELSE 'Produto Barato' END AS Status_Preco, AVG(PRECO_DE_LISTA) AS PRECO_MEDIO
+FROM tabela_de_produtos GROUP BY EMBALAGEM,
+CASE 
+WHEN preco_de_lista >= 12 THEN 'Produto Caro'
+WHEN preco_de_lista >= 7 AND preco_de_lista < 12 THEN 'Em Conta'
+ELSE 'Produto Barato' END ORDER BY embalagem;
