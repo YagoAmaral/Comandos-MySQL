05c613611240f70df10953c5010e86154ede5bdf
YagoAmaral <100030153+YagoAmaral@users.noreply.github.com>
2022-02-23 19:12:41 -0300

    Update VIEW - JOINS.sql

--- Comandos/VIEW - JOINS.sql
+++ Comandos/VIEW - JOINS.sql
@@ -1,3 +1,5 @@
+/* A função JOIN é utilizada para unir informações de outras tabelas na mesma pesquisa, tendo variações como INNER JOIN, LEFT JOIN e RIGHT JOIN. */
+
 SELECT * FROM tabela_de_vendedores A
 INNER JOIN notas_fiscais B
 ON A.MATRICULA = B.MATRICULA;
@@ -12,11 +14,11 @@ INNER JOIN itens_notas_fiscais AS I ON I.NUMERO = NF.NUMERO GROUP BY ANO;
 SELECT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A
 INNER JOIN notas_fiscais B ON A.CPF = B.CPF;
 
-SELECT distinct A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A
 INNER JOIN notas_fiscais B ON A.CPF = B.CPF;
 
-SELECT distinct A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A
-left JOIN notas_fiscais B ON A.CPF = B.CPF WHERE B.CPF IS NULL;
+SELECT DISTINCT A.CPF, A.NOME, B.CPF FROM tabela_de_clientes A
+LEFT JOIN notas_fiscais B ON A.CPF = B.CPF WHERE B.CPF IS NULL;
 
 SELECT tabela_de_vendedores.BAIRRO AS VENDEDOR_BAIRRO, 
 tabela_de_vendedores.NOME AS VENDEDOR_NOME,
@@ -29,14 +31,14 @@ SELECT tabela_de_vendedores.BAIRRO AS VENDEDOR_BAIRRO,
 tabela_de_vendedores.NOME AS VENDEDOR_NOME,
 tabela_de_clientes.BAIRRO AS CLIENTE_BAIRRO,
 tabela_de_clientes.NOME AS CLIENTE_NOME FROM tabela_de_vendedores 
-left JOIN tabela_de_clientes 
+LEFT JOIN tabela_de_clientes 
 ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO
 UNION
 SELECT tabela_de_vendedores.BAIRRO AS VENDEDOR_BAIRRO, 
 tabela_de_vendedores.NOME AS VENDEDOR_NOME,
 tabela_de_clientes.BAIRRO AS CLIENTE_BAIRRO,
 tabela_de_clientes.NOME AS CLIENTE_NOME FROM tabela_de_vendedores 
-right JOIN tabela_de_clientes 
+RIGHT JOIN tabela_de_clientes 
 ON tabela_de_vendedores.BAIRRO = tabela_de_clientes.BAIRRO;
 
 SELECT DISTINCT BAIRRO FROM tabela_de_vendedores;
